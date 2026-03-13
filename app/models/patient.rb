4675df01d025e2029da9401b7b7c6f0a8708c380
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2026-03-13 23:00:13 +0900

    feat: Add presence and uniqueness validations for hospital_id, name, and date_of_birth to Patient model.

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,6 @@
 class Patient < ApplicationRecord
   has_many :surgeries, dependent: :destroy
+  validates :hospital_id, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :date_of_birth, presence: true
 end
