be11b92993d1ed44b1569b49ea34745673931459
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2026-05-09 01:50:42 +0900

    Revert "feat: configure codespaces devcontainer development environment with Docker and Capybara support"
    This reverts commit a002eb4e61a2d87f4e500f91d65a340a21a8f8d6.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,19 +1,10 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  if ENV["CAPYBARA_SERVER_PORT"].present?
-    served_by host: "rails-app", port: ENV["CAPYBARA_SERVER_PORT"]
-
-    driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ], options: {
-      browser: :remote,
-      url: "http://#{ENV["SELENIUM_HOST"]}:4444"
-    }
-  else
-    driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ] do |options|
-      options.add_argument("--no-sandbox")
-      options.add_argument("--disable-dev-shm-usage")
-      options.add_argument("--disable-gpu")
-      options.add_argument("--window-size=1400,1400")
-    end
+  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ] do |options|
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1400,1400")
   end
 end
