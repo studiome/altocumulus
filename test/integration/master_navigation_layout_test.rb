e547dc6c3bf3f8c6744135d45f39f125d2f84dfc
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2026-09-03 10:43:47 +0900

    Fix master navigation stacking

--- test/integration/master_navigation_layout_test.rb
+++ test/integration/master_navigation_layout_test.rb
@@ -1,17 +1,25 @@
 require "test_helper"
 
 class MasterNavigationLayoutTest < ActionDispatch::IntegrationTest
-  test "master navigation uses an unclipped daisyUI dropdown with working links" do
+  test "master navigation uses an unclipped daisyUI dropdown with aligned trigger and working links" do
     get root_path
 
     assert_select "li.dropdown.dropdown-end", 1
     assert_select "li.dropdown.dropdown-end details", 0
-    assert_select "li.dropdown.dropdown-end > button.btn.btn-ghost[type='button'][tabindex='0']", text: "Masters"
+    assert_select "li.dropdown.dropdown-end > button[type='button'][tabindex='0']", text: "Masters"
+    assert_select "li.dropdown.dropdown-end > button.btn", 0
+    assert_select "li.dropdown.dropdown-end > button.btn-ghost", 0
     assert_select "li.dropdown.dropdown-end > ul.dropdown-content.menu", 1
 
     assert_select "li.dropdown.dropdown-end > ul > li > a[href='#{diagnoses_path}']", text: "Diagnoses"
     assert_select "li.dropdown.dropdown-end > ul > li > a[href='#{surgery_procedures_path}']", text: "Procedures"
     assert_select "li.dropdown.dropdown-end > ul > li > a[href='#{elective_slot_rules_path}']", text: "Slot Rules"
     assert_select "li.dropdown.dropdown-end > ul > li > a[href='#{holidays_path}']", text: "Holidays"
+  end
+
+  test "navigation layer is above page content" do
+    tailwind_source = File.read(Rails.root.join("app/assets/tailwind/application.css"))
+
+    assert_includes tailwind_source, "@apply navbar relative z-50"
   end
 end
